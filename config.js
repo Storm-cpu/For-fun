882dd563f2135079d067d8936cc7cd937ecc2d0a
Storm-cpu <77232390+Storm-cpu@users.noreply.github.com>
2021-08-17 14:57:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Hey Bae!!!',
+    introDesc: `
+    Anh có thể hỏi em vài câu được không? Trả lời thật lòng nhé 🥺`,
+    btnIntro: 'Okay ❤',
+    title: 'Có thể em không phải cô gái đặc biệt giữa đám đông. Nhưng em là cô gái đặc biệt trong lòng anh!',
+    desc: 'Thế giới của anh bừng sáng khi em xuất hiện. Làm người yêu anh nhé?',
+    btnYes: 'Okay luôn anh êu <33',
+    btnNo: 'No nhé chàng trai :3',
+    question:'Trên thế giới có hơn 8 tỷ người...sao em lại chọn anh?',
+    btnReply: 'Gữi cho anh 😋',
+    reply: 'Yêu thì yêu mà không yêu cũng...yêu <33333333',
+    mess: 'Anh biết mà 🥰. I love you 9999',
+    messDesc: 'Inbox với anh tý nhé',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/100034490890960'
+}
